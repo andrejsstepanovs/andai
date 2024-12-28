f9821ff1745f175949a9a948e99c30682025a152
Andrejs Stepanovs <andrejs.stepanovs@zalando.de>
2024-12-28 23:14:15 +0100

    container

--- cmd/setup/database/token.go
+++ cmd/setup/database/token.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"database/sql"
-	"errors"
 	"fmt"
 
 	"github.com/andrejsstepanovs/andai/pkg/redmine"
@@ -18,60 +16,33 @@ func NewGetTokenCommand(redmine *redmine.Model) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Get redmine admin token or creates it if missing")
 
-			db, err := sql.Open("mysql", viper.GetString("redmine.db"))
+			admin, err := redmine.ApiAdmin()
 			if err != nil {
-				panic(err)
-			}
-
-			const ADMIN_USER_ID = 1
-			results, err := db.Query("SELECT id, value FROM tokens WHERE action = ? AND user_id = ?", "api", ADMIN_USER_ID)
-			if err != nil {
-				fmt.Println("Redmine Admin Fail")
 				return fmt.Errorf("error redmine admin: %v", err)
 			}
-			defer results.Close()
+			fmt.Println("Admin ID:", admin.Id)
 
-			type Row struct {
-				ID    int
-				Value string
+			token, err := redmine.DbGetToken(admin.Id)
+			if err != nil {
+				return fmt.Errorf("db err: %v", err)
 			}
-			var rows []Row
-
-			// Loop through rows, using Scan to assign column data to struct fields.
-			for results.Next() {
-				var row Row
-				if err := results.Scan(&row.ID, &row.Value); err != nil {
-					return err
-				}
-				rows = append(rows, row)
-			}
-			if err = results.Err(); err != nil {
-				return err
-			}
-
-			for _, row := range rows {
-				fmt.Println("Token:", row.Value)
-			}
-			if len(rows) > 0 {
+			if token.ID > 0 {
 				fmt.Println("Token already exists")
+				fmt.Println("Token:", token.Value)
 				return nil
 			}
 
-			result, err := db.Exec("INSERT INTO tokens (value, action, user_id, created_on, updated_on) VALUES (?, ?, ?, NOW(), NOW())", viper.GetString("redmine.api_key"), "api", ADMIN_USER_ID)
+			err = redmine.DbCreateApiToken(admin.Id, viper.GetString("redmine.api_key"))
 			if err != nil {
-				return fmt.Errorf("insert settings token db err: %v", err)
+				return fmt.Errorf("after created err: %v", err)
 			}
+			fmt.Println("New token created")
 
-			affected, err := result.RowsAffected()
+			token, err = redmine.DbGetToken(admin.Id)
 			if err != nil {
-				return fmt.Errorf("rows affected err: %v", err)
+				panic(err)
 			}
-
-			if affected > 0 {
-				fmt.Println("Admin token set")
-			} else {
-				return errors.New("Admin token not created")
-			}
+			fmt.Println("Token:", token.Value)
 
 			return nil
 		},
--- pkg/redmine/model.go
+++ pkg/redmine/model.go
@@ -11,6 +11,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+const ADMIN_LOGIN = "admin"
+
 type Model struct {
 	db  *sql.DB
 	api *redmine.Client
@@ -73,6 +75,59 @@ func (c *Model) ApiGetUsers() ([]redmine.User, error) {
 	return users, nil
 }
 
+func (c *Model) DbCreateApiToken(userId int, tokenValue string) error {
+	result, err := c.db.Exec("INSERT INTO tokens (value, action, user_id, created_on, updated_on) VALUES (?, ?, ?, NOW(), NOW())", tokenValue, "api", userId)
+	if err != nil {
+		return fmt.Errorf("insert settings token db err: %v", err)
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("rows affected err: %v", err)
+	}
+	if affected == 0 {
+		return errors.New("token not created")
+	}
+	return nil
+}
+
+func (c *Model) DbGetToken(userId int) (models.Token, error) {
+	results, err := c.db.Query("SELECT id, action, value FROM tokens WHERE action = ? AND user_id = ?", "api", userId)
+	if err != nil {
+		return models.Token{}, fmt.Errorf("error redmine admin: %v", err)
+	}
+	defer results.Close()
+
+	var rows []models.Token
+	for results.Next() {
+		var row models.Token
+		if err := results.Scan(&row.ID, &row.Action, &row.Value); err != nil {
+			return models.Token{}, err
+		}
+		rows = append(rows, row)
+	}
+	err = results.Err()
+	if err != nil && !errors.As(err, &sql.ErrNoRows) {
+		return models.Token{}, err
+	}
+	for _, row := range rows {
+		return row, nil
+	}
+	return models.Token{}, nil
+}
+
+func (c *Model) ApiAdmin() (redmine.User, error) {
+	users, err := c.DbGetAllUsers()
+	if err != nil {
+		return redmine.User{}, fmt.Errorf("error redmine db get users: %v", err)
+	}
+	for _, user := range users {
+		if user.Login == ADMIN_LOGIN {
+			return user, nil
+		}
+	}
+	return redmine.User{}, errors.New("admin not found")
+}
+
 func (c *Model) DbSettingsAdminMustNotChangePassword() error {
 	result, err := c.db.Exec("UPDATE users SET must_change_passwd = 0 WHERE login = ?", "admin")
 	if err != nil {
@@ -108,7 +163,8 @@ func (c *Model) DbGetSettings(settingName string) ([]models.Settings, error) {
 		}
 		rows = append(rows, row)
 	}
-	if err = results.Err(); err != nil {
+	err = results.Err()
+	if err != nil && !errors.As(err, &sql.ErrNoRows) {
 		return nil, err
 	}
 	return rows, nil
