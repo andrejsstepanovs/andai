f52ad5ba8e79c6884b87bd9563a53c6825ab9b9b
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2025-01-29 15:56:15 +0100

    test

--- test.sh
+++ test.sh
@@ -20,6 +20,11 @@ PROJECT=test make work # will create 2 Step tasks and put them in Initial, final
 PROJECT=test make work # will move Task from QA
 
 PROJECT=test build/andai issue move "Init repository" "Approved" # move parent task to Approved
-PROJECT=test build/andai work triggers
+PROJECT=test build/andai work triggers # Will move children to Backlog
+PROJECT=test make work # will move children to Analysis (if not dependent)
+
+PROJECT=test make work # will work on 1 child from Analysis and move to In Progress
+PROJECT=test make work # will move task to QA
+PROJECT=test make work # will move task from QA to Approved
 
 #PROJECT=test bin/andai move-children # will move created children from Initial to Backlog
